13b08ae8bc6e116bea11f540e0e58b5f830039a9
AyaAbdelmoghith <105630381+AyaAbdelmoghith@users.noreply.github.com>
2023-07-28 05:09:29 +0300

    main.js

--- js/main.js
+++ js/main.js
@@ -4,31 +4,45 @@ const loginBtn = document.getElementById('loginBtn');
 const inputValidation = document.querySelector(".input-validation");
 const emailValidation = document.querySelector(".email-validation");
 const passValidation = document.querySelector(".pass-validation");
-let usersData = JSON.parse(localStorage.getItem('usersList'));
+const emptyUsersList = document.querySelector(".empty-users-list");
+let usersData = [];
 let user;
 loginBtn.addEventListener('click', checkStatus);
 function checkStatus(event) {
     event.preventDefault();
-    emptyFeilds();
-    let index = inputValidationFunc();
-    if (index !== -1) {
-        inputValidation.style.display = 'none';
-        clearFieldsStyle();
-        window.location.assign(`home.html?userIndex=${index}`);
+    if (email.value.length === 0 && password.value.length === 0)
+        emptyFeilds();
+    else {
+        if (isDataExist()) {
+            let index = inputValidationFunc();
+            if (index !== -1) {//means user is registered
+                inputValidation.style.display = 'none';
+                clearFieldsStyle();
+                window.location.assign(`home.html?userIndex=${index}`);
+            }
+            else //means user is not registered so incorect username or password
+                inputValidation.style.display = 'block';
+        }
+        else
+            emptyUsersList.style.display = 'block';//means no list for any user.
     }
-    else if (index === -1 && email.value.length !== 0 && password.value.length !== 0)
-        inputValidation.style.display = 'block';
 }
 
 function emptyFeilds() {
+    inputValidation.style.display = 'none';
+    emptyUsersList.style.display = 'none';
     if (email.value.length === 0)
         emailValidation.style.display = 'block';
-    else
-        emailValidation.style.display = 'none';
     if (password.value.length === 0)
         passValidation.style.display = 'block';
+}
+function isDataExist() {
+    if (localStorage.getItem('usersList') !== null) {
+        usersData = JSON.parse(localStorage.getItem('usersList'));
+        return true;
+    }
     else
-        passValidation.style.display = 'none';
+        return false;
 }
 function inputValidationFunc() {
     const inputEmail = email.value.trim().toLowerCase();
@@ -51,16 +65,26 @@ password.addEventListener('input', passValidationFunc);
 
 function emailValidationFunc() {
     email.style.borderBottomColor = 'green';
-    if (email.value.length === 0)
+    if (email.value.length === 0) {
         emailValidation.style.display = 'block';
-    else
+        inputValidation.style.display = 'none';
+        emptyUsersList.style.display = 'none';
+
+    }
+    else {
         emailValidation.style.display = 'none';
+    }
 
 }
 function passValidationFunc() {
     password.style.borderBottomColor = 'green';
-    if (password.value.length === 0)
+    if (password.value.length === 0) {
         passValidation.style.display = 'block';
-    else
+        inputValidation.style.display = 'none';
+        emptyUsersList.style.display = 'none';
+
+    }
+    else {
         passValidation.style.display = 'none';
+    }
 }
